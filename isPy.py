2646bcf497e6fb58ca9468468e5988c94d47ea39
Ben Leonard-Lagarde <Kernel-ROM@users.noreply.github.com>
2015-10-08 19:49:46 +0100

    Update isPy.py

--- isPy.py
+++ isPy.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python
+import sys, os, time, logging
+
+try:
+	import pyHook, pythoncom
+except:
+	print "Either pyHook or pythoncom modules are missing!"
+
+#logfile = "logs.txt"
+
+def OnKeyboardEvent(event):
+    print 'MessageName:',event.MessageName
+    print 'Message:',event.Message
+    print 'Time:',event.Time
+    print 'Window:',event.Window
+    print 'WindowName:',event.WindowName
+    print 'Ascii:', event.Ascii, chr(event.Ascii)
+    print 'Key:', event.Key
+    print 'KeyID:', event.KeyID
+    print 'ScanCode:', event.ScanCode
+    print 'Extended:', event.Extended
+    print 'Injected:', event.Injected
+    print 'Alt', event.Alt
+    print 'Transition', event.Transition
+    print '---'
+
+# return True to pass the event to other handlers
+    return True
+
+# create a hook manager
+hm = pyHook.HookManager()
+# watch for all mouse events
+hm.KeyDown = OnKeyboardEvent
+# set the hook
+hm.HookKeyboard()
+# wait forever
+pythoncom.PumpMessages()
