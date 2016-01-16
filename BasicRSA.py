0394913ddf51fe39129628b2ee619475922f3518
Ben Leonard-Lagarde <leonardlagardeb@gmail.com>
2016-01-16 19:52:14 +0000

    RSA example
    For educational purposes

---
+++ BasicRSA.py
@@ -0,0 +1,65 @@
+import random
+from fractions import gcd
+
+def Demo():
+	# pre-computed primes
+	primes = [11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+	47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+
+	# choose two randomly, ensuring they're different
+	p = primes[random.randrange(len(primes)-1)]
+	primes.remove(p)
+	q = primes[random.randrange(len(primes)-1)]
+
+	print "Step 1: p=%d & q=%d" % (p,q)
+	# Determine the product of p & q
+	ProductOfPrime = p*q
+	print "\nStep 2: p*q = %d" % ProductOfPrime
+
+	#Due to q&q being prime, totient is prime-1
+	totient = (p-1) * (q-1)
+	print "\nStep 3: Totient = %d" % totient
+
+	plist = []
+	# Determine which numbers in range have only one common divisor
+	for i in range(2, totient+1):
+		# gcd = Greatest Common Divisor
+		if (gcd(i, totient) == 1):
+			plist.append(i)
+	print plist
+	# Public(e) is then chosen randomly from list of ints
+	PublicKey = plist[random.randrange(len(plist)-1)]
+	print "\nStep 4: Random Public Key = %d" % PublicKey
+
+	# Ensure private(d) is larger than p or q
+	# so start i from the largerof the two
+	if p > q:
+		i = p
+	else:
+		i = q
+	while 1:
+		# compute private(d) so that d*e = 1 mod totient 
+		if (PublicKey * i) % totient == 1:
+			break
+		else:
+			i = i+1
+	PrivateKey = i
+	print "\nStep 5: Private Key = %d" % PrivateKey
+
+	ciphertext = Encrypt(plaintext, PublicKey, ProductOfPrime)
+	decryptext = Decrypt(ciphertext, PrivateKey, ProductOfPrime)
+
+	print "\n\nPlaintext is %d" % plaintext
+	print "\nEncrypted text is %d" % ciphertext
+	print "\nDecrypted text is %d" % decryptext
+	return
+
+def Encrypt(plaintext, publicKey, PoP):
+	return(plaintext**publicKey % PoP)
+
+def Decrypt(ciphertext, privateKey, PoP):
+	return(ciphertext**privateKey % PoP)
+
+# Choose random plaintext int 1-99
+plaintext = random.randrange(1,99)
+Demo()
